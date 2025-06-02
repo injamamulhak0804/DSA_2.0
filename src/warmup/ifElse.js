8cbc187fd2d988f47299fca412659b9d15ac81ce
injamamulhak0804 <injamamulhak080404@gmail.com>
2025-06-02 21:27:09 +0530

    feat: Study IfElse - Wramup

--- src/warmup/ifElse.js
+++ src/warmup/ifElse.js
@@ -1,5 +1,29 @@
-function ifElse() {}
+// function EligibleToVote(age) {
+//   if (age < 1) {
+//     console.log("Invalid Input");
+//     return;
+//   }
+//   if (age < 18) {
+//     console.log("Not Eligible For Vote");
+//     return;
+//   }
+//   console.log("Eligible for vote");
+// }
+// EligibleToVote(18);
+// EligibleToVote(10);
+// EligibleToVote(1);
+// EligibleToVote(0);
 
-ifElse();
+// function OddOrEven(num) {
+//   if (num % 2 != 0) {
+//     console.log("The Given Number is odd");
+//   } else {
+//     console.log("The Given Number Even");
+//   }
+// }
 
-module.export = ifElse;
+// OddOrEven(23);
+// OddOrEven(3);
+// OddOrEven(89);
+// OddOrEven(12);
+// OddOrEven(90);
