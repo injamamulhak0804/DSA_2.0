d5e175a6ed13d70fe01f21cfa5bfc462761ed547
injamamulhak0804 <injamamulhak080404@gmail.com>
2025-06-16 18:27:41 +0530

    feat: Study SingleNumber - Array-Level-0

---
+++ src/Array-Level-0/Single_Number.js
@@ -0,0 +1,21 @@
+function SingleNumber(arr) {
+  let hash = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!hash[arr[i]]) {
+      hash[arr[i]] = 1;
+    } else {
+      hash[arr[i]]++;
+    }
+  }
+
+  //   console.log("hash length: ", hash);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (hash[arr[i]] == 1) {
+      console.log(arr[i]);
+    }
+  }
+}
+
+SingleNumber([1, 2, 3, 4, 3, 2, 1]);
