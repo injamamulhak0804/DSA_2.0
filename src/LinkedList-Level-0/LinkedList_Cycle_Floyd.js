2ddf0ec62c1f925b6a6c97a4cc3d1a7ea2499b0b
thinkmetal-zamam <hak@thinkmetal.in>
2025-08-01 10:40:29 +0530

    feat: Study LinkedList_Cycle_Floyd LinkedList  - 0

---
+++ src/LinkedList-Level-0/LinkedList_Cycle_Floyd.js
@@ -0,0 +1,10 @@
+function LinkedList_Cycle_Floyd(head) {
+    let slow = fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) return true;
+    }
+    return false
+}
+LinkedList_Cycle_Floyd(9)
